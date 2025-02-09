7c9590e68b034e417db1464d862639322632c786
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-02-08 21:11:37 -0300

    desenvolvendo os conceitos de orientação a ojetos, sempre é bom para nosso crescimento e aprofundamet, pois o estuo se multiplica n trabalho

--- classes1.py
+++ classes1.py
@@ -11,6 +11,9 @@ class Veiculo:
     def qtde_rodas(self):
         return self._qtde_rodas
     
+    def alerta(self):
+        return 'Aleta da classe pai'
+    
 class Motocicleta(Veiculo):
     def __init__(self, cor, qtde_rodas, estilo: str):
         super().__init__(cor, qtde_rodas)
@@ -21,4 +24,34 @@ class Motocicleta(Veiculo):
         return self._estilo
     
     def acelera(self):
-        return 'Acelerar'
+        return 'Acelerar motociclta'
+    
+    def alerta(self):
+        return super().alerta()
+    
+class Carro(Veiculo):
+    def __init__(self, cor: str, qtde_rodas: int, marca: str) -> None:
+        super().__init__(cor, qtde_rodas)
+        self._marca = marca
+
+    @property
+    def marca(self):
+        return self._marca
+
+    def acelerar(self):
+        return 'Acelerando o carro.....'
+    
+class Caminhao(Veiculo):
+    def __init__(self, cor, qtde_rodas, montadora: str):
+        super().__init__(cor, qtde_rodas)
+        self._motadora = montadora
+
+    @property
+    def montadora(self):
+        return self._motadora
+    
+    def acelerar(self):
+        return 'Acelerando o carro.....'
+    
+    def acelerar(self):
+        return 'Acelerando o caminhão.....'
--- script1.py
+++ script1.py
@@ -1,7 +1,31 @@
-from classes1 import Motocicleta
+from classes1 import Motocicleta, Carro, Caminhao
 
 moto = Motocicleta('Preta', 2, 'custom')
 
 print(moto.cor)
 print(moto.qtde_rodas)
 print(moto.estilo)
+
+carro = Carro('preta', 4, 'Nissan')
+caminhao = Caminhao('azul', 12, 'Scania')
+
+print(carro.cor)
+print(caminhao.acelerar())
+print(carro.acelerar())
+
+print('Polimorfismo')
+
+lista = [1,2,3]
+
+print(len(lista))
+
+nome = 'Python'
+print(len(nome))
+
+for i in nome:
+    print(i)
+
+
+print(2 + 2)
+
+print('a' + 'b')
